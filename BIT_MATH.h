b9376f1c70bf93b9b7ab500935e8752ce3a26a59
yehiaibrahim <yehianumb@gmail.com>
2023-04-17 05:38:26 +0200

    Add files via upload

---
+++ BIT_MATH.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define SET_BIT(VAR,BIT) VAR |=(1<<BIT)
+#define CLR_BIT(VAR,BIT) VAR &=~(1<<BIT)
+#define TOG_BIT(VAR,BIT) VAR ^=(1<<BIT)
+#define GET_BIT(VAR,BIT) (VAR>>BIT) & 1
---
+++ STD_TYPES.h
@@ -0,0 +1,12 @@
+#pragma once
+
+typedef unsigned char u8;
+typedef signed char s8;
+
+typedef unsigned short int u16;
+typedef signed short int s16;
+
+typedef unsigned long int u32;
+typedef signed long int s32;
+
+typedef float f32;
